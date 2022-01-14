bbc1b3f92ab038382550827b2ff4e14dde802a85
Bart Naessens <>
2022-01-14 10:38:50 +0100

    feat(archiefidp): use expand variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { AuthModule } from '~modules/auth';
 			envFilePath: '.env/.env.local',
 			load: [config],
 			validationSchema: configValidationSchema,
+			expandVariables: true,
 		}),
 		AuthModule,
 	],
