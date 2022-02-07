ba586252fdd4dde0211c6eabc89aad5620598a36
Bart Naessens <>
2022-02-07 11:06:31 +0100

    feat(arc-278): add tests

--- src/modules/spaces/controllers/spaces.controller.spec.ts
+++ src/modules/spaces/controllers/spaces.controller.spec.ts
@@ -4,7 +4,23 @@ import { SpacesService } from '../services/spaces.service';
 
 import { SpacesController } from './spaces.controller';
 
-import { DataModule } from '~modules/data';
+const mockSpacesResponse = {
+	items: [
+		{
+			id: '1',
+			name: 'Space Mountain',
+		},
+		{
+			id: '2',
+			name: 'Space X',
+		},
+	],
+};
+
+const mockSpacesService = {
+	findAll: jest.fn(),
+	findById: jest.fn(),
+};
 
 describe('SpacesController', () => {
 	let spacesController: SpacesController;
@@ -12,8 +28,13 @@ describe('SpacesController', () => {
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [SpacesController],
-			imports: [DataModule],
-			providers: [SpacesService], // TODO mock?
+			imports: [],
+			providers: [
+				{
+					provide: SpacesService,
+					useValue: mockSpacesService,
+				},
+			],
 		}).compile();
 
 		spacesController = module.get<SpacesController>(SpacesController);
@@ -21,5 +42,21 @@ describe('SpacesController', () => {
 
 	it('should be defined', () => {
 		expect(spacesController).toBeDefined();
+	});
+
+	describe('getSpaces', () => {
+		it('should return all spaces', async () => {
+			mockSpacesService.findAll.mockResolvedValueOnce(mockSpacesResponse);
+			const spaces = await spacesController.getSpaces(null);
+			expect(spaces.items.length).toEqual(2);
+		});
+	});
+
+	describe('getSpaceById', () => {
+		it('should return a space by id', async () => {
+			mockSpacesService.findById.mockResolvedValueOnce(mockSpacesResponse.items[0]);
+			const space = await spacesController.getSpaceById('1');
+			expect(space.id).toEqual('1');
+		});
 	});
 });
