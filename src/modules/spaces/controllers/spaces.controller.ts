bf02a1ee105993a75a8dfee5b91d4f293430f403
Bart Naessens <>
2022-02-09 12:04:32 +0100

    feat(swagger): fixed tag

--- src/modules/spaces/controllers/spaces.controller.ts
+++ src/modules/spaces/controllers/spaces.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Logger, Param, ParseUUIDPipe, Query } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { IPagination } from '@studiohyperdrive/pagination';
 
 import { SpacesQueryDto } from '../dto/spaces.dto';
@@ -14,7 +14,6 @@ export class SpacesController {
 	constructor(private spacesService: SpacesService) {}
 
 	@Get()
-	@ApiBody({ type: SpacesQueryDto, required: false })
 	public async getSpaces(@Query() query: SpacesQueryDto): Promise<IPagination<Space>> {
 		const spaces = await this.spacesService.findAll(query);
 		return spaces;
