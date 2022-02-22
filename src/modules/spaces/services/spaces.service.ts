364eb6f1294b184e1c27e99fdc8c6e6b415be693
Bert Verhelst <verhelstbert@gmail.com>
2022-02-22 19:21:20 +0100

    Update src/modules/spaces/services/spaces.service.ts

--- src/modules/spaces/services/spaces.service.ts
+++ src/modules/spaces/services/spaces.service.ts
@@ -24,9 +24,9 @@ export class SpacesService {
 			id: get(graphQlSpace, 'id'),
 			maintainerId: get(graphQlSpace, 'schema_maintainer.schema_identifier'),
 			name: get(graphQlSpace, 'schema_maintainer.schema_name'),
-			description: !isEmpty(graphQlSpace.schema_description)
-				? graphQlSpace.schema_description
-				: get(graphQlSpace, 'schema_maintainer.information[0].description'),
+			description: isEmpty(graphQlSpace.schema_description)
+				? graphQlSpace.schema_maintainer?.information?.[0]?.description,
+				: graphQlSpace.schema_description
 			serviceDescription: get(graphQlSpace, 'schema_service_description'),
 			image: get(graphQlSpace, 'schema_image'),
 			color: get(graphQlSpace, 'schema_color'),
