0bc9d79413469d529ebccdfcc34aa509d2e91071
Bert Verhelst <verhelstbert@gmail.com>
2022-02-28 19:36:48 +0100

    feat(arc-283): add routes for getting notifications and marking as read

---
+++ src/modules/notifications/services/queries.gql.ts
@@ -0,0 +1,59 @@
+export const FIND_NOTIFICATIONS_BY_USER = `
+	query getNotificationsForUser($userProfileId: uuid, $moreRecentThan: timestamp, $offset: Int, $limit: Int) {
+		app_notification(where: {_or: [{recipient: {_eq: $userProfileId}, show_at: {_gt: $moreRecentThan}, status: {_eq: "READ"}}, {recipient: {_eq: $userProfileId}, status: {_eq: "UNREAD"}}]}, order_by: {show_at: desc, status: asc}, limit: $limit, offset: $offset) {
+			id
+			description
+			title
+			status
+			notification_type
+			recipient
+			visit_id
+			show_at
+			created_at
+			updated_at
+		}
+		app_notification_aggregate(where: {_or: [{recipient: {_eq: $userProfileId}, show_at: {_gt: $moreRecentThan}, status: {_eq: "READ"}}, {recipient: {_eq: $userProfileId}, status: {_eq: "UNREAD"}}]}) {
+			aggregate {
+				count
+			}
+		}
+	}
+`;
+
+export const INSERT_NOTIFICATION = `
+	mutation insertNotification($object: app_notification_insert_input!) {
+		insert_app_notification(objects: [$object]) {
+			returning {
+				id
+				description
+				title
+				status
+				notification_type
+				recipient
+				visit_id
+				show_at
+				created_at
+				updated_at
+			}
+		}
+	}
+`;
+
+export const UPDATE_NOTIFICATION = `
+	mutation updateNotification($notificationId: uuid, $userProfileId: uuid, $notification: app_notification_set_input) {
+		update_app_notification(where: {id: {_eq: $notificationId}, recipient: {_eq: $userProfileId}}, _set: $notification) {
+			returning {
+				id
+				description
+				title
+				status
+				notification_type
+				recipient
+				visit_id
+				show_at
+				created_at
+				updated_at
+			}
+		}
+	}
+`;
