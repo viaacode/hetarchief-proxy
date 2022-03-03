9425b486508130f112f089d8936ccf4f6aa12b6b
Bert Verhelst <verhelstbert@gmail.com>
2022-03-03 17:21:58 +0100

    feat(arc-430): add notifications when a visit request is approved/denied

--- src/modules/visits/controllers/visits.controller.spec.ts
+++ src/modules/visits/controllers/visits.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { addHours } from 'date-fns';
 
 import { VisitsService } from '../services/visits.service';
 import { VisitStatus } from '../types';
@@ -7,18 +8,44 @@ import { VisitsController } from './visits.controller';
 
 import { NotificationsService } from '~modules/notifications/services/notifications.service';
 import { SpacesService } from '~modules/spaces/services/spaces.service';
+import { AudienceType, Space } from '~modules/spaces/types';
 import { User } from '~modules/users/types';
 import { SessionHelper } from '~shared/auth/session-helper';
 
+const mockVisit1 = {
+	id: '93eedf1a-a508-4657-a942-9d66ed6934c2',
+	spaceId: '3076ad4b-b86a-49bc-b752-2e1bf34778dc',
+	userProfileId: 'df8024f9-ebdc-4f45-8390-72980a3f29f6',
+	timeframe: 'Binnen 3 weken donderdag van 5 to 6',
+	reason: 'Ik wil graag deze zaal bezoeken 7',
+	status: 'APPROVED',
+	startAt: '2022-03-03T16:00:00',
+	endAt: '2022-03-03T17:00:00',
+	createdAt: '2022-02-11T15:28:40.676',
+	updatedAt: '2022-02-11T15:28:40.676',
+	visitorName: 'Marie Odhiambo',
+	visitorMail: 'marie.odhiambo@example.com',
+	visitorId: 'df8024f9-ebdc-4f45-8390-72980a3f29f6',
+};
+
+const mockVisit2 = {
+	id: '40f3f893-ba4f-4bc8-a871-0d492172134d',
+	spaceId: '24ddc913-3e03-42ea-9bd1-ba486401bc30',
+	userProfileId: 'df8024f9-ebdc-4f45-8390-72980a3f29f6',
+	timeframe: 'Binnen 3 weken donderdag van 5 to 6',
+	reason: 'Ik wil graag deze zaal bezoeken 2',
+	status: 'PENDING',
+	startAt: '2022-03-03T16:00:00',
+	endAt: '2022-03-03T17:00:00',
+	createdAt: '2022-02-11T15:28:40.676',
+	updatedAt: '2022-02-11T15:28:40.676',
+	visitorName: 'Marie Odhiambo',
+	visitorMail: 'marie.odhiambo@example.com',
+	visitorId: 'df8024f9-ebdc-4f45-8390-72980a3f29f6',
+};
+
 const mockVisitsResponse = {
-	items: [
-		{
-			id: 'visit-1',
-		},
-		{
-			id: 'visit-2',
-		},
-	],
+	items: [mockVisit1, mockVisit2],
 };
 
 const mockUser: User = {
@@ -27,6 +54,34 @@ const mockUser: User = {
 	lastName: 'Testers',
 	email: 'test.testers@meemoo.be',
 	acceptedTosAt: '1997-01-01T00:00:00.000Z',
+};
+
+const mockSpace: Space = {
+	id: '52caf5a2-a6d1-4e54-90cc-1b6e5fb66a21',
+	maintainerId: 'OR-154dn75',
+	name: 'Amsab-ISG',
+	description:
+		'Amsab-ISG is het Instituut voor Sociale Geschiedenis. Het bewaart, ontsluit, onderzoekt en valoriseert het erfgoed van sociale en humanitaire bewegingen.',
+	serviceDescription: null,
+	image: null,
+	color: null,
+	logo: 'https://assets.viaa.be/images/OR-154dn75',
+	audienceType: AudienceType.PUBLIC,
+	publicAccess: false,
+	contactInfo: {
+		email: null,
+		telephone: null,
+		address: {
+			street: 'Pijndersstraat 28',
+			postalCode: '9000',
+			locality: 'Gent',
+			postOfficeBoxNumber: null,
+		},
+	},
+	isPublished: false,
+	publishedAt: null,
+	createdAt: '2022-01-13T13:10:14.41978',
+	updatedAt: '2022-01-13T13:10:14.41978',
 };
 
 const mockVisitsService: Partial<Record<keyof VisitsService, jest.SpyInstance>> = {
@@ -43,6 +98,7 @@ const mockNotificationsService: Partial<Record<keyof NotificationsService, jest.
 
 const mockSpacesService: Partial<Record<keyof SpacesService, jest.SpyInstance>> = {
 	getMaintainerProfileIds: jest.fn(),
+	findById: jest.fn(),
 };
 
 describe('VisitsController', () => {
@@ -113,12 +169,15 @@ describe('VisitsController', () => {
 			expect(mockSpacesService.getMaintainerProfileIds).toBeCalledTimes(1);
 			expect(mockNotificationsService.createForMultipleRecipients).toBeCalledTimes(1);
 			sessionHelperSpy.mockRestore();
+			mockSpacesService.getMaintainerProfileIds.mockClear();
+			mockNotificationsService.createForMultipleRecipients.mockClear();
 		});
 	});
 
 	describe('update', () => {
 		it('should update a visit', async () => {
 			mockVisitsService.update.mockResolvedValueOnce(mockVisitsResponse.items[0]);
+			mockSpacesService.findById.mockResolvedValueOnce(mockSpace);
 			const sessionHelperSpy = jest
 				.spyOn(SessionHelper, 'getArchiefUserInfo')
 				.mockReturnValue(mockUser);
@@ -126,6 +185,8 @@ describe('VisitsController', () => {
 			const visit = await visitsController.update(
 				'visit-id',
 				{
+					startAt: new Date().toISOString(),
+					endAt: addHours(new Date(), 2).toISOString(),
 					status: VisitStatus.APPROVED,
 				},
 				{}
@@ -133,6 +194,68 @@ describe('VisitsController', () => {
 
 			expect(visit).toEqual(mockVisitsResponse.items[0]);
 			sessionHelperSpy.mockRestore();
+			mockNotificationsService.create.mockClear();
+		});
+
+		it('should update a visit status: approved', async () => {
+			mockVisitsService.update.mockResolvedValueOnce(mockVisit1);
+			mockSpacesService.findById.mockResolvedValueOnce(mockSpace);
+			const sessionHelperSpy = jest
+				.spyOn(SessionHelper, 'getArchiefUserInfo')
+				.mockReturnValue(mockUser);
+
+			const visit = await visitsController.update(
+				mockVisit1.id,
+				{
+					status: VisitStatus.APPROVED,
+				},
+				{}
+			);
+			expect(visit).toEqual(mockVisit1);
+			expect(mockNotificationsService.create).toHaveBeenCalledTimes(1);
+			sessionHelperSpy.mockRestore();
+			mockNotificationsService.create.mockClear();
+		});
+
+		it('should update a visit status: denied', async () => {
+			mockVisitsService.update.mockResolvedValueOnce(mockVisit1);
+			mockSpacesService.findById.mockResolvedValueOnce(mockSpace);
+			const sessionHelperSpy = jest
+				.spyOn(SessionHelper, 'getArchiefUserInfo')
+				.mockReturnValue(mockUser);
+
+			const visit = await visitsController.update(
+				mockVisit1.id,
+				{
+					status: VisitStatus.DENIED,
+				},
+				{}
+			);
+			expect(visit).toEqual(mockVisit1);
+			expect(mockNotificationsService.create).toHaveBeenCalledTimes(1);
+			sessionHelperSpy.mockRestore();
+			mockNotificationsService.create.mockClear();
+		});
+
+		it('should update a visit status: cancelled', async () => {
+			mockVisitsService.update.mockResolvedValueOnce(mockVisit1);
+			mockSpacesService.findById.mockResolvedValueOnce(mockSpace);
+			const sessionHelperSpy = jest
+				.spyOn(SessionHelper, 'getArchiefUserInfo')
+				.mockReturnValue(mockUser);
+
+			const visit = await visitsController.update(
+				mockVisit1.id,
+				{
+					status: VisitStatus.CANCELLED_BY_VISITOR,
+				},
+				{}
+			);
+
+			expect(visit).toEqual(mockVisit1);
+			expect(mockNotificationsService.create).toHaveBeenCalledTimes(0);
+			sessionHelperSpy.mockRestore();
+			mockNotificationsService.create.mockClear();
 		});
 	});
 });
