e39fa949acdc3e25c52544ca61d953a2cd8bd3f7
Bart Naessens <>
2022-03-21 14:49:53 +0100

    fix(arc-333): fix test coverage

--- src/modules/campaign-monitor/services/campaign-monitor.service.spec.ts
+++ src/modules/campaign-monitor/services/campaign-monitor.service.spec.ts
@@ -103,6 +103,16 @@ describe('CampaignMonitorService', () => {
 			expect(result).toBeTruthy();
 		});
 
+		it('should log and not send to an empty recipients email adres', async () => {
+			const visit = getMockVisit();
+			const result = await campaignMonitorService.sendForVisit({
+				to: [{ id: visit.visitorId, email: null }],
+				template: Template.VISIT_APPROVED,
+				visit: getMockVisit(),
+			});
+			expect(result).toBeFalsy();
+		});
+
 		it('should NOT call the campaign monitor if the template was not found', async () => {
 			const visit = getMockVisit();
 			const result = await campaignMonitorService.sendForVisit({
