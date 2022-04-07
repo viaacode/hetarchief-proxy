1f4f08a27a58ec3a48b37a791943b017b362f619
Bart Naessens <>
2022-04-07 11:49:36 +0200

    fix(arc-303): flipped boolean logic

--- src/modules/auth/services/idp.service.ts
+++ src/modules/auth/services/idp.service.ts
@@ -43,30 +43,31 @@ export class IdpService {
 		// permissions check
 		const apps = get(ldapUser, 'attributes.apps', []);
 		if (
-			!apps.includes('hetarchief') &&
-			!apps.includes('admins') // TODO replace by a single value 'hetarchief-beheer' once archief 2.0 is launched
+			apps.includes('hetarchief') ||
+			apps.includes('admins') // TODO replace by a single value 'hetarchief-beheer' once archief 2.0 is launched
 		) {
-			// no member of hetarchief-beheer
-			// TOP-section of the flowchart
-			// check for kiosk permissions -- otherwise it's a regular user
-			if (get(ldapUser, 'attributes.organizationalStatus', []).includes('kiosk')) {
-				// organization needs to have a space to be a kiosk user
-				const maintainerId = get(ldapUser, 'attributes.o[0]');
-				if (await this.spacesService.findBySlug(maintainerId)) {
-					return Group.KIOSK_VISITOR;
-				}
+			// bottom section of the flowchart
+			const maintainerId = get(ldapUser, 'attributes.o[0]');
+			if (maintainerId && (await this.spacesService.findBySlug(maintainerId))) {
+				return Group.CP_ADMIN;
+			}
+
+			if (IdpService.ADMIN_ORGANISATIONS.includes(maintainerId)) {
+				return Group.MEEMOO_ADMIN;
 			}
 
 			return Group.VISITOR;
 		}
-		// bottom section of the flowchart
-		const maintainerId = get(ldapUser, 'attributes.o[0]');
-		if (maintainerId && (await this.spacesService.findBySlug(maintainerId))) {
-			return Group.CP_ADMIN;
-		}
 
-		if (IdpService.ADMIN_ORGANISATIONS.includes(maintainerId)) {
-			return Group.MEEMOO_ADMIN;
+		// no member of hetarchief-beheer
+		// TOP-section of the flowchart
+		// check for kiosk permissions -- otherwise it's a regular user
+		if (get(ldapUser, 'attributes.organizationalStatus', []).includes('kiosk')) {
+			// organization needs to have a space to be a kiosk user
+			const maintainerId = get(ldapUser, 'attributes.o[0]');
+			if (await this.spacesService.findBySlug(maintainerId)) {
+				return Group.KIOSK_VISITOR;
+			}
 		}
 
 		return Group.VISITOR;
