18390b93c3a471cf4f88392883d87710f81af944
Bart Naessens <>
2022-05-18 11:31:35 +0200

    fix(arc-610): fix data permission test

--- src/modules/data/controllers/data.controller.ts
+++ src/modules/data/controllers/data.controller.ts
@@ -4,11 +4,12 @@ import { ApiTags } from '@nestjs/swagger';
 import { GraphQlQueryDto } from '../dto/graphql-query.dto';
 import { DataService } from '../services/data.service';
 
-import { SessionUserEntity } from '~modules/users/classes/session-user';
+// import { SessionUserEntity } from '~modules/users/classes/session-user';
 import { Group, GroupIdToName, Permission, User } from '~modules/users/types';
 import { Idp } from '~shared/auth/auth.types';
-import { SessionUser } from '~shared/decorators/user.decorator';
+// import { SessionUser } from '~shared/decorators/user.decorator';
 
+// TODO Admin core: fix
 const mockUser: User = {
 	id: 'd285a546-b42b-4fb3-bfa7-ef8be9208bc0',
 	firstName: 'Meemoo',
@@ -29,8 +30,8 @@ export class DataController {
 
 	@Post()
 	public async post(
-		@Body() dataQueryDto: GraphQlQueryDto,
-		@SessionUser() user: SessionUserEntity
+		@Body() dataQueryDto: GraphQlQueryDto
+		// @SessionUser() user: SessionUserEntity
 	): Promise<any> {
 		// return this.dataService.executeClientQuery(user.getUser(), dataQueryDto);
 		return this.dataService.executeClientQuery(mockUser, dataQueryDto);
