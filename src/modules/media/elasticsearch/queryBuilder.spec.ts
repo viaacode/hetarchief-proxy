6561e55178d12c461be3c57f9e492f248dfd5ed5
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-06-10 15:30:53 +0200

    fix(arc-957): fix test to check should match array in es query

--- src/modules/media/elasticsearch/queryBuilder.spec.ts
+++ src/modules/media/elasticsearch/queryBuilder.spec.ts
@@ -78,7 +78,7 @@ describe('QueryBuilder', () => {
 				size: 10,
 				page: 1,
 			});
-			expect(esQuery.query.bool.must.length).toBeGreaterThanOrEqual(2);
+			expect(esQuery.query.bool.must[0].bool.should.length).toBeGreaterThanOrEqual(2);
 		});
 
 		it('should return an empty query when empty query filter is specified', () => {
