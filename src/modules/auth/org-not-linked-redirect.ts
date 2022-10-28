cdf6ca8e326f0997798869871e16708211a9a474
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2022-10-28 16:11:51 +0200

    fix(arc-976): fix redirect kiosk user without space linked

--- src/modules/auth/org-not-linked-redirect.ts
+++ src/modules/auth/org-not-linked-redirect.ts
@@ -10,14 +10,6 @@ import { Idp } from '~shared/auth/auth.types';
  * We logout the user to ensure new info is fetched the next time they login.
  * Since the maintainer will probably contact meemoo about the problem and meemoo will add the organisation to the account
  * So the next time they login, we need to fetch fresh info from the respective IDP (hetarchief | meemoo)
- *
- * This throws an error:
- * UnhandledPromiseRejectionWarning: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
- * But if we use the normal nestjs way of redirecting: return {url, statusCode} then nestjs tries to redirect to logout as a post method
- * Which throws a 404 not found
- *
- * This way it works, but it also throws the error :s
- * Note: Doesn't seem to work locally
  */
 export function orgNotLinkedLogoutAndRedirectToErrorPage(
 	res: Response,
@@ -25,9 +17,9 @@ export function orgNotLinkedLogoutAndRedirectToErrorPage(
 	idp: Idp,
 	message: string,
 	title: string
-) {
-	return res.redirect(
-		queryString.stringifyUrl({
+): { url: string } {
+	return {
+		url: queryString.stringifyUrl({
 			url: `${proxyHost}/auth/${idp.toLowerCase()}/logout`,
 			query: {
 				returnToUrl: queryString.stringifyUrl({
@@ -36,6 +28,6 @@ export function orgNotLinkedLogoutAndRedirectToErrorPage(
 				}),
 				forceLogout: 'true',
 			},
-		})
-	);
+		}),
+	};
 }
