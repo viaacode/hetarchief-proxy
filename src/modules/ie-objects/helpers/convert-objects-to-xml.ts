f5d814119d8e3130378f03de26a87d0ceae70423
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-03-06 15:11:12 +0100

    refactor(arc-1425): refactor xml object

--- src/modules/ie-objects/helpers/convert-objects-to-xml.ts
+++ src/modules/ie-objects/helpers/convert-objects-to-xml.ts
@@ -5,11 +5,11 @@ import { IeObject } from '../ie-objects.types';
 export const convertObjectsToXml = (objects: Partial<IeObject>[]): string => {
 	// this structure defines the parent 'objects' tag, which includes
 	// all objects wrapped in a separate 'object' tag
-	const csvData = objects.map(({ accessThrough, ...csvData }) => csvData);
-	return convert.js2xml({ objects: { object: csvData } }, { compact: true, spaces: 2 });
+	const xmlData = objects.map(({ accessThrough, ...xmlData }) => xmlData);
+	return convert.js2xml({ objects: { object: xmlData } }, { compact: true, spaces: 2 });
 };
 
 export const convertObjectToXml = (object: Partial<IeObject>): string => {
-	const { accessThrough, ...csvData } = object;
-	return convert.js2xml({ csvData }, { compact: true, spaces: 2 });
+	const { accessThrough, ...xmlData } = object;
+	return convert.js2xml({ object: xmlData }, { compact: true, spaces: 2 });
 };
