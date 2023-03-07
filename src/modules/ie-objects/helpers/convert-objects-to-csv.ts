15aa75ffaf060802610b368c5d8cfea7f2ec9e46
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-03-07 09:34:21 +0100

    feat(arc-1425): remove flat npm package

--- src/modules/ie-objects/helpers/convert-objects-to-csv.ts
+++ src/modules/ie-objects/helpers/convert-objects-to-csv.ts
@@ -1,14 +1,26 @@
-import { flatten } from 'flat';
 import Papa from 'papaparse';
+import { chain, fromPairs, map, toPairs } from 'ramda';
 
 import { IeObject } from '../ie-objects.types';
 
 export const convertObjectsToCsv = (objects: Partial<IeObject>[]): string => {
 	const csvData = objects.map(({ accessThrough, ...csvData }) => csvData);
-	return Papa.unparse([flatten(csvData)], { delimiter: ';' });
+	return Papa.unparse([flattenObj(csvData)], { delimiter: ';' });
 };
 
 export const convertObjectToCsv = (object: Partial<IeObject>): string => {
 	const { accessThrough, ...csvData } = object;
-	return Papa.unparse([flatten(csvData)], { delimiter: ';' });
+	return Papa.unparse([flattenObj(csvData)], { delimiter: ';' });
+};
+
+const flattenObj = (obj) => {
+	const go = (objX): any =>
+		chain(([k, v]: [string, any]) => {
+			if (typeof v === 'object') {
+				return map(([kX, vX]) => [`${k}.${kX}`, vX], go(v));
+			} else {
+				return [[k, v]];
+			}
+		}, toPairs(objX));
+	return fromPairs(go(obj));
 };
