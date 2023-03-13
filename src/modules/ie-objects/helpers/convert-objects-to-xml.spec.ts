e23e5a4da4add9651b661543e92e5e2d0bdc53af
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-03-13 12:53:25 +0100

    feat(arc-1474): add tests for xml helper

--- src/modules/ie-objects/helpers/convert-objects-to-xml.spec.ts
+++ src/modules/ie-objects/helpers/convert-objects-to-xml.spec.ts
@@ -1,4 +1,11 @@
 import { IeObject } from '../ie-objects.types';
+import {
+	mockIeObjectWithMetadataSetALL,
+	mockIeObjectWithMetadataSetALLWithEssence,
+	mockIeObjectWithMetadataSetALLXml,
+	mockIeObjectWithMetadataSetLTD,
+	mockIeObjectWithMetadataSetLTDXml,
+} from '../mocks/ie-objects.mock';
 
 import { convertObjectToXml } from './convert-objects-to-xml';
 
@@ -6,5 +13,20 @@ describe('convertObjectToXml', () => {
 	it('returns the xml version of an object', () => {
 		const xml = convertObjectToXml({ meemooOriginalCp: '1' } as unknown as IeObject);
 		expect(xml.startsWith('<object>')).toBeTruthy();
+	});
+
+	it('returns the xml version of an object with metadate set LTD', () => {
+		const xml = convertObjectToXml(mockIeObjectWithMetadataSetLTD);
+		expect(xml).toEqual(mockIeObjectWithMetadataSetLTDXml);
+	});
+	it('returns the xml version of an object with metadate set ALL', () => {
+		const xml = convertObjectToXml(mockIeObjectWithMetadataSetALL);
+		expect(xml).toEqual(mockIeObjectWithMetadataSetALLXml);
+	});
+	it('returns the xml version of an object with metadate set ALL with Essence', () => {
+		const xml = convertObjectToXml(
+			mockIeObjectWithMetadataSetALLWithEssence as unknown as IeObject
+		);
+		expect(xml).toEqual(mockIeObjectWithMetadataSetALLXml);
 	});
 });
