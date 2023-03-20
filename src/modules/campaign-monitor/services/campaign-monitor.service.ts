eb1b0784f92d5a74acf63a66e8d6cf2ca081f62b
Brecht Tourlousse <brecht.tourlousse@studiohyperdrive.be>
2023-03-20 13:47:15 +0100

    fix(arc-1191): remove two times api version

--- src/modules/campaign-monitor/services/campaign-monitor.service.ts
+++ src/modules/campaign-monitor/services/campaign-monitor.service.ts
@@ -183,10 +183,10 @@ export class CampaignMonitorService {
 			}
 
 			url = `/${process.env.CAMPAIGN_MONITOR_SUBSCRIBER_API_VERSION as string}/${
-				process.env.CAMPAIGN_MONITOR_SUBSCRIBER_API_VERSION as string
-			}/${process.env.CAMPAIGN_MONITOR_SUBSCRIBER_API_ENDPOINT}/${
-				process.env.CAMPAIGN_MONITOR_OPTIN_LIST_05 as string
-			}.json/?${queryString.stringify({ email: user.getMail() })}`;
+				process.env.CAMPAIGN_MONITOR_SUBSCRIBER_API_ENDPOINT
+			}/${process.env.CAMPAIGN_MONITOR_OPTIN_LIST_05 as string}.json/?${queryString.stringify(
+				{ email: user.getMail() }
+			)}`;
 
 			const mappedPreferences = [];
 
