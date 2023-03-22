fc912c32b836c6ab70be0c13b34d67fd9238d7f3
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-03-22 09:55:11 +0100

    feat(arc-1504): controller test

--- src/modules/campaign-monitor/controllers/campaign-monitor.controller.spec.ts
+++ src/modules/campaign-monitor/controllers/campaign-monitor.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { Configuration } from '~config';
 
+import { mockUser } from '../mocks/campaign-monitor.mocks';
 import { CampaignMonitorService } from '../services/campaign-monitor.service';
 
 import { CampaignMonitorController } from './campaign-monitor.controller';
@@ -12,6 +13,8 @@ import { TestingLogger } from '~shared/logging/test-logger';
 const mockCampaignMonitorService: Partial<Record<keyof CampaignMonitorService, jest.SpyInstance>> =
 	{
 		sendTransactionalMail: jest.fn(),
+		fetchNewsletterPreferences: jest.fn(),
+		updateNewsletterPreferences: jest.fn(),
 	};
 
 const mockConfigService: Partial<Record<keyof ConfigService, jest.SpyInstance>> = {
@@ -61,6 +64,33 @@ describe('CampaignMonitorController', () => {
 			});
 
 			expect(sent).toBeTruthy();
+		});
+	});
+	describe('getPreferences', () => {
+		it('get user newsletter preferences', async () => {
+			mockCampaignMonitorService.fetchNewsletterPreferences.mockResolvedValueOnce({
+				newsletter: true,
+			});
+
+			const sent = await campaignMonitorController.getPreferences({
+				email: 'test@studiohyperdrive.be',
+			});
+
+			expect(sent).toEqual({ newsletter: true });
+		});
+	});
+	describe('getPreferences', () => {
+		it('get user newsletter preferences', async () => {
+			mockCampaignMonitorService.updateNewsletterPreferences.mockResolvedValueOnce({
+				newsletter: true,
+			});
+
+			const sent = await campaignMonitorController.updatePreferences(
+				{ newsletter: true },
+				mockUser
+			);
+
+			// expect(sent).toEqual({ newsletter: true });
 		});
 	});
 });
