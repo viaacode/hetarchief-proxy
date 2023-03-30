594811198163175324cf531dd63f4693b2a27c0a
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-03-30 16:17:17 +0200

    feat(arc-1571): update mockdata

--- src/modules/ie-objects/mocks/ie-objects.mock.ts
+++ src/modules/ie-objects/mocks/ie-objects.mock.ts
@@ -23,6 +23,8 @@ export const mockIeObject: IeObject = {
 	maintainerName: 'vrt',
 	maintainerSlug: 'vrt',
 	maintainerLogo: 'https://assets.viaa.be/images/OR-rf5kf25',
+	maintainerDescription:
+		'De Vlaamse Radio- en Televisieomroeporganisatie, afgekort VRT, is de Nederlandstalige openbare omroep voor radio en televisie in België.',
 	durationInSeconds: null,
 	numberOfPages: null,
 	datePublished: '2020-09-01',
@@ -88,6 +90,8 @@ export const mockIeObjectWithMetadataSetLTD: Partial<IeObject> = {
 	maintainerName: 'vrt',
 	maintainerSlug: 'vrt',
 	maintainerLogo: 'https://assets.viaa.be/images/OR-rf5kf25',
+	maintainerDescription:
+		'De Vlaamse Radio- en Televisieomroeporganisatie, afgekort VRT, is de Nederlandstalige openbare omroep voor radio en televisie in België.',
 	name: 'Durf te vragen R002 A0001',
 	series: null,
 	programs: null,
@@ -136,6 +140,8 @@ export const mockIeObjectWithMetadataSetALL: Partial<IeObject> = {
 	maintainerName: 'vrt',
 	maintainerSlug: 'vrt',
 	maintainerLogo: 'https://assets.viaa.be/images/OR-rf5kf25',
+	maintainerDescription:
+		'De Vlaamse Radio- en Televisieomroeporganisatie, afgekort VRT, is de Nederlandstalige openbare omroep voor radio en televisie in België.',
 	name: 'Durf te vragen R002 A0001',
 	series: null,
 	programs: null,
@@ -195,6 +201,8 @@ export const mockIeObjectWithMetadataSetALLWithEssence: Partial<IeObject> = {
 	maintainerName: 'vrt',
 	maintainerSlug: 'vrt',
 	maintainerLogo: 'https://assets.viaa.be/images/OR-rf5kf25',
+	maintainerDescription:
+		'De Vlaamse Radio- en Televisieomroeporganisatie, afgekort VRT, is de Nederlandstalige openbare omroep voor radio en televisie in België.',
 	name: 'Durf te vragen R002 A0001',
 	series: null,
 	programs: null,
