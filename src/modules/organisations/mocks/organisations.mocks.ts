48b8917118a1c0dfad0e59de8bbacf2a61c81b97
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-04-03 16:20:14 +0200

    feat(arc-1579): test updateOrganisationsCache method

--- src/modules/organisations/mocks/organisations.mocks.ts
+++ src/modules/organisations/mocks/organisations.mocks.ts
@@ -1,4 +1,9 @@
-import { GqlOrganisation, Organisation } from '../organisations.types';
+import {
+	GqlOrganisation,
+	Organisation,
+	OrganisationInfoV2,
+	OrganisationResponse,
+} from '../organisations.types';
 
 import { IeObjectSector } from '~modules/ie-objects/ie-objects.types';
 
@@ -58,4 +63,44 @@ export const mockOrganisation: Organisation = {
 	updatedAt: '2023-04-03T13:28:01.434203+02:00',
 	sector: IeObjectSector.PUBLIC,
 	formUrl: undefined,
+};
+
+export const mockOrganisationInfoV2: OrganisationInfoV2 = {
+	id: 'OR-rf5kf25',
+	label: 'VRT',
+	description:
+		'De Vlaamse Radio- en Televisieomroeporganisatie, afgekort VRT, is de Nederlandstalige openbare omroep voor radio en televisie in België.',
+	sector: IeObjectSector.PUBLIC,
+	form_url:
+		'https://www.vrt.be/heb-je-een-vraag/s/contactsupport-vrtarchief?name_user={first_name}&mail_user={email}&local_id={local_cp_id}&viaa_id={pid}&surname_user={last_name}&title={title}&serie={title_serie}',
+	logo: {
+		iri: 'https://assets.viaa.be/images/OR-rf5kf25',
+	},
+	contact_point: [
+		{
+			contact_type: 'ontsluiting',
+			telephone: '+32 2 741 37 20',
+			email: 'vrtarchief@vrt.be',
+		},
+		{
+			contact_type: 'facturatie',
+			telephone: null,
+			email: null,
+		},
+	],
+	primary_site: {
+		address: null,
+	},
+};
+
+export const getMockOrganisationResponse = (amount: number): OrganisationResponse => {
+	const mockData = {
+		data: {
+			contentpartners: [],
+		},
+	};
+	for (let i = 0; i < amount; i++) {
+		mockData.data.contentpartners.push(mockOrganisationInfoV2);
+	}
+	return mockData;
 };
--- src/modules/organisations/services/organisations.service.spec.ts
+++ src/modules/organisations/services/organisations.service.spec.ts
@@ -1,10 +1,15 @@
 import { DataService } from '@meemoo/admin-core-api';
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import nock from 'nock';
 
 import { Configuration } from '~config';
 
-import { mockGqlOrganisation, mockOrganisation } from '../mocks/organisations.mocks';
+import {
+	getMockOrganisationResponse,
+	mockGqlOrganisation,
+	mockOrganisation,
+} from '../mocks/organisations.mocks';
 
 import { OrganisationsService } from './organisations.service';
 
@@ -12,8 +17,8 @@ import { TestingLogger } from '~shared/logging/test-logger';
 
 const mockConfigService = {
 	get: jest.fn((key: keyof Configuration): string | boolean => {
-		if (key === 'PROXY_API_KEY') {
-			return 'fakeApiKey';
+		if (key === 'ORGANIZATIONS_API_V2_URL') {
+			return 'http://fake-organisations-url.be';
 		}
 
 		return key;
@@ -25,8 +30,11 @@ const mockDataService: Partial<Record<keyof DataService, jest.SpyInstance>> = {
 
 describe('OrganisationService', () => {
 	let organisationsService: OrganisationsService;
+	const env = process.env;
 
 	beforeEach(async () => {
+		process.env.ORGANIZATIONS_API_V2_URL = 'http://fake-organisations-url.be';
+
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [
 				OrganisationsService,
@@ -48,6 +56,7 @@ describe('OrganisationService', () => {
 
 	afterEach(() => {
 		mockDataService.execute.mockRestore();
+		process.env = env;
 	});
 
 	describe('findOrganisationBySchemaIdentifier', () => {
@@ -79,10 +88,134 @@ describe('OrganisationService', () => {
 	});
 
 	describe('adapt', () => {
-		it('successfully adapt a GqlOrganisation to an Organisation', async () => {
+		it('should successfully adapt a GqlOrganisation to an Organisation', async () => {
 			const result = organisationsService.adapt(mockGqlOrganisation);
 
 			expect(result).toEqual(mockOrganisation);
+		});
+	});
+
+	describe('updateOrganisationsCache', () => {
+		it('should successfully updates the organisations cache', async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(51));
+
+			mockDataService.execute.mockResolvedValueOnce(true); // execute in emptyOrganizations method
+			mockDataService.execute.mockResolvedValueOnce(Promise.resolve()); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.updateOrganisationsCache();
+			} catch (err) {
+				expect(err).toBeUndefined();
+			}
+		});
+
+		it("should throw InternalServerErrorException when there are less than 50 cp's", async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(0));
+
+			mockDataService.execute.mockResolvedValueOnce(true); // execute in emptyOrganizations method
+			mockDataService.execute.mockResolvedValueOnce(Promise.resolve()); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.updateOrganisationsCache();
+				fail('updateOrganisationsCache should have thrown an error');
+			} catch (err) {
+				expect(err.name).toEqual('InternalServerErrorException');
+				expect(err.message).toEqual('Failed to make update organization cache');
+				expect(err.response.innerException.message).toEqual(
+					'Request to organizations api was unsuccessful'
+				);
+			}
+		});
+
+		it('should throw InternalServerErrorException when organizations api throws an error', async () => {
+			nock('http://fake-organisations-url.be').post('/').replyWithError('');
+
+			mockDataService.execute.mockResolvedValueOnce(true); // execute in emptyOrganizations method
+			mockDataService.execute.mockResolvedValueOnce(Promise.resolve()); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.updateOrganisationsCache();
+				fail('updateOrganisationsCache should have thrown an error');
+			} catch (err) {
+				expect(err.name).toEqual('InternalServerErrorException');
+				expect(err.message).toEqual('Failed to make update organization cache');
+				expect(err.response.innerException.name).toEqual('RequestError');
+			}
+		});
+
+		it('should throw InternalServerErrorException when emptyOrganizations() throws an error', async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(51));
+
+			mockDataService.execute.mockRejectedValueOnce(''); // execute in emptyOrganizations method
+			mockDataService.execute.mockResolvedValueOnce(Promise.resolve()); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.updateOrganisationsCache();
+				fail('updateOrganisationsCache should have thrown an error');
+			} catch (err) {
+				expect(err.name).toEqual('InternalServerErrorException');
+				expect(err.message).toEqual('Failed to make update organization cache');
+				expect(err.response.innerException.message).toEqual(
+					'Failed to empty organizations'
+				);
+			}
+		});
+		it('should throw InternalServerErrorException when insertOrganizations() throws an error', async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(51));
+
+			mockDataService.execute.mockResolvedValueOnce(true); // execute in emptyOrganizations method
+			mockDataService.execute.mockRejectedValueOnce(''); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.updateOrganisationsCache();
+				fail('updateOrganisationsCache should have thrown an error');
+			} catch (err) {
+				expect(err.name).toEqual('InternalServerErrorException');
+				expect(err.message).toEqual('Failed to make update organization cache');
+				expect(err.response.innerException.message).toEqual(
+					'Failed to insert organizations'
+				);
+			}
+		});
+	});
+	describe('onApplicationBootstrap', () => {
+		it('should succesfully cache organisations', async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(51));
+
+			mockDataService.execute.mockResolvedValueOnce(true); // execute in emptyOrganizations method
+			mockDataService.execute.mockResolvedValueOnce(Promise.resolve()); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.onApplicationBootstrap();
+			} catch (err) {
+				expect(err).toBeUndefined();
+			}
+		});
+
+		it('should throw an error when in fails to cache organisations', async () => {
+			nock('http://fake-organisations-url.be')
+				.post('/')
+				.reply(200, getMockOrganisationResponse(0));
+
+			mockDataService.execute.mockRejectedValueOnce(''); // execute in emptyOrganizations method
+			mockDataService.execute.mockRejectedValueOnce(''); // execute in insertOrganizations method
+
+			try {
+				await organisationsService.onApplicationBootstrap();
+				fail('onApplicationBootstrap should have logged an error');
+			} catch (err) {
+				expect(err).toBeDefined();
+			}
 		});
 	});
 });
