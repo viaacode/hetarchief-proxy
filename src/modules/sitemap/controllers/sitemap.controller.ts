592b7bbc9de61912de77c848de09572f05cd2f26
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-04-18 14:14:35 +0200

    refactor(arc-1559): make code cleaner

--- src/modules/sitemap/controllers/sitemap.controller.ts
+++ src/modules/sitemap/controllers/sitemap.controller.ts
@@ -1,30 +1,14 @@
-import { Readable } from 'stream';
-
-import { ContentPagesService } from '@meemoo/admin-core-api';
 import { Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import moment from 'moment';
 
 import { SitemapService } from '../services/sitemap.service';
-import { SitemapItemConfig, SitemapItemInfo } from '../sitemap.types';
 
-import { VisitorSpaceStatus } from '~generated/database-aliases';
-import { AssetsService } from '~modules/assets/services/assets.service';
 import { AssetFileType } from '~modules/assets/types';
-import { IeObjectLicense } from '~modules/ie-objects/ie-objects.types';
-import { IeObjectsService } from '~modules/ie-objects/services/ie-objects.service';
-import { SpacesService } from '~modules/spaces/services/spaces.service';
 
 @ApiTags('Sitemap')
 @Controller('sitemap')
 export class SitemapController {
-	constructor(
-		private sitemapService: SitemapService,
-		private spacesService: SpacesService,
-		private ieObjectsService: IeObjectsService,
-		private assetsService: AssetsService,
-		private contentPagesService: ContentPagesService
-	) {}
+	constructor(private sitemapService: SitemapService) {}
 
 	@Post()
 	public async getSitemap(): Promise<string> {
@@ -37,154 +21,7 @@ export class SitemapController {
 						-zoekpagina per cp: '/zoeken/?aanbieders={or id van aanbieder}' done
 						-item-detailpagina’s: '/zoeken/{maintainerSlug}/{objectSchemaId}' done
 		*/
-		this.generateSitemap(); // no await because this can take a while and we don't want FE to crash
+		this.sitemapService.generateSitemap(); // no await because this can take a while and we don't want FE to crash
 		return `${process.env.ASSET_SERVER_ENDPOINT}/${process.env.ASSET_SERVER_BUCKET_NAME}/${AssetFileType.SITEMAP}/index`;
-	}
-
-	private async generateSitemap() {
-		const sitemapConfig = await this.sitemapService.getSitemapConfig();
-		const contentPagesPaths = await this.sitemapService.getContentPagesPaths();
-
-		const staticPages = ['/', '/bezoek', '/geheime-content-pagina']; // /geheime-content-pagina is for testing
-
-		const activeSpaces = await this.spacesService.findAll(
-			{ size: 1000, status: [VisitorSpaceStatus.Active] },
-			null
-		);
-
-		const publicObjects = await this.ieObjectsService.findObjectsForSitemap([
-			IeObjectLicense.PUBLIEK_METADATA_LTD,
-			IeObjectLicense.PUBLIEK_METADATA_ALL,
-		]);
-
-		// Create sitemap info for general pages
-		const generalPages: SitemapItemInfo[] = SitemapController.blacklistAndPrioritizePages(
-			[
-				...staticPages.map((path) => ({
-					loc: process.env.CLIENT_HOST + path,
-					changefreq: 'monthly',
-				})),
-				...contentPagesPaths.map((path) => ({
-					loc: process.env.CLIENT_HOST + path,
-					changefreq: 'monthly',
-				})),
-				// /zoeken is seperate because of the required order
-				{
-					loc: process.env.CLIENT_HOST + '/zoeken',
-					changefreq: 'monthly',
-				},
-				...activeSpaces.items.map((space) => ({
-					loc: process.env.CLIENT_HOST + '/zoeken/?aanbieders=' + space.maintainerId,
-					changefreq: 'weekly',
-					lastmod: moment(space.updatedAt).format('YYYY-MM-DD'),
-				})),
-			],
-			sitemapConfig
-		);
-
-		// Create sitemap info for item detail pages
-		const itemDetailPages: SitemapItemInfo[] = SitemapController.blacklistAndPrioritizePages(
-			[
-				...publicObjects.map((object) => ({
-					loc:
-						process.env.CLIENT_HOST +
-						'/zoeken/' +
-						object.maintainerSlug +
-						'/' +
-						object.schemaIdentifier,
-					changefreq: 'weekly',
-					lastmod: moment(object.updatedAt).format('YYYY-MM-DD'),
-				})),
-			],
-			sitemapConfig
-		);
-		const XML_OBJECTS_SIZE = 50; // amount of objects in 1 xml file, should be 50000
-		const itemDetailPagesArray = []; // This contains arrays each containing a maximum of XML_OBJECTS_SIZE items
-
-		for (let i = 0; i < itemDetailPages.length; i += XML_OBJECTS_SIZE) {
-			const chunk = itemDetailPages.slice(i, i + XML_OBJECTS_SIZE);
-			itemDetailPagesArray.push(chunk);
-		}
-
-		// Generate and upload all the xml files
-		const xmlUrls = [];
-
-		// Render general xml
-		const renderedGeneralXml = `<?xml version="1.0" encoding="UTF-8"?>
-		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-			${generalPages.map(SitemapController.renderPage).join('\n')}
-		</urlset>
-		`;
-		xmlUrls.push(await this.uploadXml(renderedGeneralXml, 'general'));
-
-		// Generate itemDetail xml
-		await Promise.all(
-			itemDetailPagesArray.map(async (pages: SitemapItemInfo[], index) => {
-				const renderedXml = `<?xml version="1.0" encoding="UTF-8"?>
-				<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-					${pages.map(SitemapController.renderPage).join('\n')}
-				</urlset>
-				`;
-				xmlUrls.push(await this.uploadXml(renderedXml, `itemDetail${index}`));
-			})
-		);
-
-		// Generate index xml
-		const indexXml = `<?xml version="1.0" encoding="UTF-8"?>
-		<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-			${xmlUrls.map(SitemapController.renderIndexPage).join('\n')}
-		</sitemapindex>
-		`;
-		await this.uploadXml(indexXml, `index`);
-	}
-
-	private static renderPage(pageInfo: SitemapItemInfo): string {
-		return `<url>
-			<loc>${pageInfo.loc}</loc>
-			${pageInfo.lastmod ? `<lastmod>${pageInfo.lastmod}</lastmod>` : ``}
-			<changefreq>${pageInfo.changefreq}</changefreq>
-			${pageInfo.priority ? `<priority>${pageInfo.priority}</priority>` : ``}
-			</url>`;
-	}
-
-	private static renderIndexPage(url: string): string {
-		return `<sitemap>
-			<loc>${url}</loc>
-			</sitemap>`;
-	}
-	private async uploadXml(xml: string, name: string): Promise<string> {
-		return await this.assetsService.uploadSitemap(AssetFileType.SITEMAP, {
-			fieldname: name,
-			originalname: name,
-			encoding: '',
-			mimetype: 'text/xml',
-			size: 0,
-			stream: new Readable(),
-			destination: '',
-			filename: name,
-			path: '',
-			buffer: Buffer.from(xml, 'utf-8'),
-		});
-	}
-
-	// This function blacklists and adds priority using the config json in the database
-	private static blacklistAndPrioritizePages(
-		pages: SitemapItemInfo[],
-		config: SitemapItemConfig
-	): SitemapItemInfo[] {
-		const configPaths = config.value.map((c) => c.path);
-
-		return pages.reduce((result, page) => {
-			const path = page.loc.substring(process.env.CLIENT_HOST.length);
-			if (configPaths.includes(path)) {
-				const configValue = config.value.find((c) => c.path === path);
-				if (configValue?.blacklisted === true) {
-					return result;
-				} else {
-					return [...result, { ...page, priority: configValue?.priority }];
-				}
-			}
-			return [...result, page];
-		}, []);
 	}
 }
--- src/modules/sitemap/services/sitemap.service.ts
+++ src/modules/sitemap/services/sitemap.service.ts
@@ -1,19 +1,128 @@
+import { Readable } from 'stream';
+
 import { ContentPagesService, DataService } from '@meemoo/admin-core-api';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import moment from 'moment';
 
-import { SitemapItemConfig } from '../sitemap.types';
+import { SitemapItemConfig, SitemapItemInfo } from '../sitemap.types';
 
+import { VisitorSpaceStatus } from '~generated/database-aliases';
 import {
 	GetSitemapConfigDocument,
 	GetSitemapConfigQuery,
 } from '~generated/graphql-db-types-hetarchief';
+import { AssetsService } from '~modules/assets/services/assets.service';
+import { AssetFileType } from '~modules/assets/types';
+import { IeObjectLicense } from '~modules/ie-objects/ie-objects.types';
+import { IeObjectsService } from '~modules/ie-objects/services/ie-objects.service';
+import { SpacesService } from '~modules/spaces/services/spaces.service';
 
 @Injectable()
 export class SitemapService {
 	constructor(
 		protected dataService: DataService,
-		protected contentPagesService: ContentPagesService
+		protected spacesService: SpacesService,
+		protected contentPagesService: ContentPagesService,
+		protected ieObjectsService: IeObjectsService,
+		protected assetsService: AssetsService
 	) {}
+
+	public async generateSitemap() {
+		const sitemapConfig = await this.getSitemapConfig();
+		const contentPagesPaths = await this.getContentPagesPaths();
+
+		const staticPages = ['/', '/bezoek', '/geheime-content-pagina']; // /geheime-content-pagina is for testing
+
+		const activeSpaces = await this.spacesService.findAll(
+			{ size: 1000, status: [VisitorSpaceStatus.Active] },
+			null
+		);
+
+		const publicObjects = await this.ieObjectsService.findObjectsForSitemap([
+			IeObjectLicense.PUBLIEK_METADATA_LTD,
+			IeObjectLicense.PUBLIEK_METADATA_ALL,
+		]);
+
+		// Create sitemap info for general pages
+		const generalPages: SitemapItemInfo[] = this.blacklistAndPrioritizePages(
+			[
+				...staticPages.map((path) => ({
+					loc: process.env.CLIENT_HOST + path,
+					changefreq: 'monthly',
+				})),
+				...contentPagesPaths.map((path) => ({
+					loc: process.env.CLIENT_HOST + path,
+					changefreq: 'monthly',
+				})),
+				// /zoeken is seperate because of the required order
+				{
+					loc: process.env.CLIENT_HOST + '/zoeken',
+					changefreq: 'monthly',
+				},
+				...activeSpaces.items.map((space) => ({
+					loc: process.env.CLIENT_HOST + '/zoeken/?aanbieders=' + space.maintainerId,
+					changefreq: 'weekly',
+					lastmod: moment(space.updatedAt).format('YYYY-MM-DD'),
+				})),
+			],
+			sitemapConfig
+		);
+
+		// Create sitemap info for item detail pages
+		const itemDetailPages: SitemapItemInfo[] = this.blacklistAndPrioritizePages(
+			[
+				...publicObjects.map((object) => ({
+					loc:
+						process.env.CLIENT_HOST +
+						'/zoeken/' +
+						object.maintainerSlug +
+						'/' +
+						object.schemaIdentifier,
+					changefreq: 'weekly',
+					lastmod: moment(object.updatedAt).format('YYYY-MM-DD'),
+				})),
+			],
+			sitemapConfig
+		);
+		const XML_OBJECTS_SIZE = 50; // amount of objects in 1 xml file, should be 50000
+		const itemDetailPagesArray = []; // This contains arrays each containing a maximum of XML_OBJECTS_SIZE items
+
+		for (let i = 0; i < itemDetailPages.length; i += XML_OBJECTS_SIZE) {
+			const chunk = itemDetailPages.slice(i, i + XML_OBJECTS_SIZE);
+			itemDetailPagesArray.push(chunk);
+		}
+
+		// Generate and upload all the xml files
+		const xmlUrls = [];
+
+		// Render general xml
+		const renderedGeneralXml = `<?xml version="1.0" encoding="UTF-8"?>
+		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+			${generalPages.map(this.renderPage).join('\n')}
+		</urlset>
+		`;
+		xmlUrls.push(await this.uploadXml(renderedGeneralXml, 'general'));
+
+		// Generate itemDetail xml
+		await Promise.all(
+			itemDetailPagesArray.map(async (pages: SitemapItemInfo[], index) => {
+				const renderedXml = `<?xml version="1.0" encoding="UTF-8"?>
+				<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+					${pages.map(this.renderPage).join('\n')}
+				</urlset>
+				`;
+				xmlUrls.push(await this.uploadXml(renderedXml, `itemDetail${index}`));
+			})
+		);
+
+		// Generate index xml
+		const indexXml = `<?xml version="1.0" encoding="UTF-8"?>
+		<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+			${xmlUrls.map(this.renderIndexPage).join('\n')}
+		</sitemapindex>
+		`;
+		await this.uploadXml(indexXml, `index`);
+	}
 
 	public async getSitemapConfig(): Promise<SitemapItemConfig> {
 		try {
@@ -42,5 +151,57 @@ export class SitemapService {
 		} catch (err) {
 			throw new InternalServerErrorException('Failed getting all the content pages', err);
 		}
+	}
+
+	// Helpers
+	// ------------------------------------------------------------------------
+	private renderPage(pageInfo: SitemapItemInfo): string {
+		return `<url>
+			<loc>${pageInfo.loc}</loc>
+			${pageInfo.lastmod ? `<lastmod>${pageInfo.lastmod}</lastmod>` : ``}
+			<changefreq>${pageInfo.changefreq}</changefreq>
+			${pageInfo.priority ? `<priority>${pageInfo.priority}</priority>` : ``}
+			</url>`;
+	}
+
+	private renderIndexPage(url: string): string {
+		return `<sitemap>
+			<loc>${url}</loc>
+			</sitemap>`;
+	}
+	private async uploadXml(xml: string, name: string): Promise<string> {
+		return await this.assetsService.uploadSitemap(AssetFileType.SITEMAP, {
+			fieldname: name,
+			originalname: name,
+			encoding: '',
+			mimetype: 'text/xml',
+			size: 0,
+			stream: new Readable(),
+			destination: '',
+			filename: name,
+			path: '',
+			buffer: Buffer.from(xml, 'utf-8'),
+		});
+	}
+
+	// This function blacklists and adds priority using the config json in the database
+	private blacklistAndPrioritizePages(
+		pages: SitemapItemInfo[],
+		config: SitemapItemConfig
+	): SitemapItemInfo[] {
+		const configPaths = config.value.map((c) => c.path);
+
+		return pages.reduce((result, page) => {
+			const path = page.loc.substring(process.env.CLIENT_HOST.length);
+			if (configPaths.includes(path)) {
+				const configValue = config.value.find((c) => c.path === path);
+				if (configValue?.blacklisted === true) {
+					return result;
+				} else {
+					return [...result, { ...page, priority: configValue?.priority }];
+				}
+			}
+			return [...result, page];
+		}, []);
 	}
 }
