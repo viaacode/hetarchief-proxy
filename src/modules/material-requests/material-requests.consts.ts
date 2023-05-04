41fccfdb95c83f03deba4516096b58beffca33df
Emile Van Tichelen <emile.vantichelen@studiohyperdrive.be>
2023-05-04 13:10:32 +0200

    fix(arc-1671): add missing materialRequestOrderProps

--- src/modules/material-requests/material-requests.consts.ts
+++ src/modules/material-requests/material-requests.consts.ts
@@ -3,8 +3,10 @@ import { MaterialRequest } from '~modules/material-requests/material-requests.ty
 export const ORDER_PROP_TO_DB_PROP: Partial<Record<keyof MaterialRequest, string>> = {
 	id: 'id',
 	createdAt: 'created_at',
+	updatedAt: 'updated_at',
 	type: 'type',
 	requesterFullName: 'requested_by.full_name',
 	requesterMail: 'requested_by.mail',
 	maintainerName: 'object.maintainer.schema_name',
+	objectSchemaName: 'object.schema_name',
 };
