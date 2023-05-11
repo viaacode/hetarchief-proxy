af8680a69b39fe45ba20ae3f4a1328843b4c9e11
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-05-11 19:00:32 +0200

    fix(is-part-of): fix tests and export function for isPartOf field

--- src/modules/ie-objects/helpers/convert-objects-to-csv.ts
+++ src/modules/ie-objects/helpers/convert-objects-to-csv.ts
@@ -5,13 +5,33 @@ import Papa from 'papaparse';
 import { IE_OBJECT_PROPS_METADATA_EXPORT } from '../ie-objects.conts';
 import { IeObject } from '../ie-objects.types';
 
+/**
+ * converts isPartOf object to isPartOf.serie, isPartOfProgramma, ...
+ * @param ieObject
+ */
+function mapIeObjectForExport(ieObject: Partial<IeObject>): any {
+	// Flatten isPartOf property
+	const ieObjectCopy = _.pick(ieObject, IE_OBJECT_PROPS_METADATA_EXPORT);
+	delete ieObjectCopy.isPartOf;
+	return {
+		...ieObjectCopy,
+		...Object.fromEntries(
+			Object.entries(ieObject.isPartOf || {}).map((isPartOfPair) => [
+				`isPartOf.${isPartOfPair[0]}`,
+				isPartOfPair[1],
+			])
+		),
+	};
+}
+
 export const convertObjectsToCsv = (objects: Partial<IeObject>[]): string => {
-	const csvData = objects.map((o) => _.pick(o, IE_OBJECT_PROPS_METADATA_EXPORT));
-	return Papa.unparse([flatten(csvData)], { delimiter: ';' });
+	const csvData = objects.map(mapIeObjectForExport);
+	return Papa.unparse([flatten(csvData)], { delimiter: ';', newline: '\n' });
 };
 
-export const convertObjectToCsv = (object: Partial<IeObject>): string => {
-	return Papa.unparse([flatten(_.pick(object, IE_OBJECT_PROPS_METADATA_EXPORT))], {
+export const convertObjectToCsv = (ieObject: Partial<IeObject>): string => {
+	return Papa.unparse([flatten(mapIeObjectForExport(ieObject))], {
 		delimiter: ';',
+		newline: '\n',
 	});
 };
