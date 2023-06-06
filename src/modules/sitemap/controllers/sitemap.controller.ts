3f96b7bfe68481528a55fcc81722509809f37354
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-06-06 18:13:12 +0200

    fix(arc-1560): check api key for regenerate sitemap endpoint
    https://meemoo.atlassian.net/browse/ARC-1560

--- src/modules/sitemap/controllers/sitemap.controller.ts
+++ src/modules/sitemap/controllers/sitemap.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Headers, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { SitemapService } from '../services/sitemap.service';
 
 import { AssetFileType } from '~modules/assets/types';
+import { ApiKeyGuard } from '~shared/guards/api-key.guard';
 
 @ApiTags('Sitemap')
 @Controller('sitemap')
@@ -11,9 +12,13 @@ export class SitemapController {
 	constructor(private sitemapService: SitemapService) {}
 
 	@Post()
-	public async generateSitemap(): Promise<string> {
+	@UseGuards(ApiKeyGuard)
+	public async generateSitemap(
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		@Headers('apikey') apikey: string
+	): Promise<string> {
 		const sitemapConfig = await this.sitemapService.getSitemapConfig();
-		this.sitemapService.generateSitemap(sitemapConfig); // no await because this can take a while and we don't want FE to crash
+		this.sitemapService.generateSitemap(sitemapConfig); // no await because this can take a while, and we don't want FE to crash
 		return `${process.env.ASSET_SERVER_ENDPOINT}/${process.env.ASSET_SERVER_BUCKET_NAME}/${AssetFileType.SITEMAP}/index.xml`;
 	}
 }
