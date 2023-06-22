841cff15125cd32a9bf073c3492be2ac97cd5bee
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-06-22 10:45:20 +0200

    fix(arc-1291): limit results pagination to 10000
    https://meemoo.atlassian.net/browse/ARC-1291

--- src/modules/ie-objects/elasticsearch/elasticsearch.consts.ts
+++ src/modules/ie-objects/elasticsearch/elasticsearch.consts.ts
@@ -135,7 +135,7 @@ export const MAX_NUMBER_SEARCH_RESULTS = 2000;
 // Max count to return to the client to avoid error:
 // -  Result window is too large, from + size must be less than or equal to: [10000] but was [17110].
 // -  See the scroll api for a more efficient way to request large data sets.
-// -  This limit can be set by changing the [index.max_result_window] index level setting. // TODO Still relevant?
+// -  This limit can be set by changing the [index.max_result_window] index level setting.
 export const MAX_COUNT_SEARCH_RESULTS = 10000;
 
 export const READABLE_TO_ELASTIC_FILTER_NAMES: {
