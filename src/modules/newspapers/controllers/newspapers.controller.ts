ab18d0fc35d1a446346dc68f5695c28feb373d79
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2024-07-22 12:33:12 +0200

    feat(ARC-2168): add jpg, xml, tst and metadata to zip file for newspaper
    https://meemoo.atlassian.net/browse/ARC-2168

--- src/modules/newspapers/controllers/newspapers.controller.ts
+++ src/modules/newspapers/controllers/newspapers.controller.ts
@@ -1,3 +1,5 @@
+import https from 'https';
+
 import {
 	Controller,
 	ForbiddenException,
@@ -11,51 +13,44 @@ import {
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import archiver from 'archiver';
+import { mapLimit } from 'blend-promise-utils';
 import { Request, Response } from 'express';
-import { compact } from 'lodash';
+import { isNil } from 'lodash';
 
+import { IeObjectsController } from '~modules/ie-objects/controllers/ie-objects.controller';
 import { convertObjectToCsv } from '~modules/ie-objects/helpers/convert-objects-to-csv';
 import { convertObjectToXml } from '~modules/ie-objects/helpers/convert-objects-to-xml';
-import type { IeObject } from '~modules/ie-objects/ie-objects.types';
-import { IeObjectsService } from '~modules/ie-objects/services/ie-objects.service';
+import {
+	NEWSPAPER_MIME_TYPE_ALTO,
+	NEWSPAPER_MIME_TYPE_BROWSE_COPY,
+} from '~modules/newspapers/newspapers.consts';
 import { SessionUserEntity } from '~modules/users/classes/session-user';
 import { Permission } from '~modules/users/types';
 import { RequireAllPermissions } from '~shared/decorators/require-permissions.decorator';
 import { SessionUser } from '~shared/decorators/user.decorator';
-import { getIpFromRequest } from '~shared/helpers/get-ip-from-request';
 
 @ApiTags('Newspapers')
 @Controller('newspapers')
 export class NewspapersController {
-	constructor(private ieObjectsService: IeObjectsService) {}
+	constructor(private ieObjectsController: IeObjectsController) {}
 
 	@Get(':id/export/zip')
 	@Header('Content-Type', 'application/zip')
 	@RequireAllPermissions(Permission.DOWNLOAD_OBJECT)
 	public async downloadNewspaperAsZip(
 		@Param('id') id: string,
-		@Query('page') pageIndex: number,
+		@Query('page') pageIndex: number | undefined,
 		@Headers('referer') referer: string,
 		@Req() request: Request,
 		@Res() res: Response,
 		@SessionUser() user: SessionUserEntity
 	): Promise<void> {
-		console.log({
-			pageIndex,
-			userId: user.getId(),
-		});
-		// const limitedObjectMetadata = await this.ieObjectsController.getIeObjectById(
-		// 	id,
-		// 	referer,
-		// 	request,
-		// 	user
-		// );
-		const ieObjects: IeObject[] = await this.ieObjectsService.findBySchemaIdentifiers(
-			[id],
+		const limitedObjectMetadata = await this.ieObjectsController.getIeObjectById(
+			id,
 			referer,
-			getIpFromRequest(request)
+			request,
+			user
 		);
-		const limitedObjectMetadata = ieObjects[0];
 
 		if (!limitedObjectMetadata) {
 			throw new ForbiddenException(
@@ -69,24 +64,57 @@ export class NewspapersController {
 			);
 		}
 
-		const pageImageUrls = compact(
-			limitedObjectMetadata.representations.flatMap((representation) => {
-				return representation.files.map((file) => {
-					if (file.mimeType !== 'image/jpeg') {
-						return null;
-					}
-					return file.storedAt;
-				});
-			})
-		);
-		const metadataXml = convertObjectToXml(limitedObjectMetadata);
-		const metadataCsv = convertObjectToCsv(limitedObjectMetadata);
-		const metadataJson = JSON.stringify(limitedObjectMetadata, null, 2);
+		const zipEntries: { filename: string; type: 'url' | 'string'; value: string }[] = [];
 
-		console.log({
-			pageImageUrls,
-			metadataXml,
-			metadataJson,
+		// Extract images and alto xml urls from the object
+		const exportSinglePage = !isNil(pageIndex) && !isNaN(pageIndex);
+		const pagesToExport = exportSinglePage
+			? limitedObjectMetadata.pageRepresentations.slice(pageIndex, pageIndex + 1)
+			: limitedObjectMetadata.pageRepresentations;
+		pagesToExport.forEach((representations, pageRepresentationIndex) => {
+			return representations.forEach((representation) => {
+				return representation.files.forEach((file) => {
+					const currentPageIndex = exportSinglePage ? pageIndex : pageRepresentationIndex;
+					const pageNumber = String(currentPageIndex + 1).padStart(3, '0');
+					if (file.mimeType === NEWSPAPER_MIME_TYPE_BROWSE_COPY) {
+						zipEntries.push({
+							filename: `page-${pageNumber}.jpg`,
+							type: 'url',
+							value: file.storedAt,
+						});
+					} else if (file.mimeType === NEWSPAPER_MIME_TYPE_ALTO) {
+						zipEntries.push({
+							filename: `page-${pageNumber}--ocr-alto.xml`,
+							type: 'url',
+							value: file.storedAt,
+						});
+						zipEntries.push({
+							filename: `page-${pageNumber}--ocr-text.txt`,
+							type: 'string',
+							value: representation.schemaTranscript,
+						});
+					}
+
+					return null;
+				});
+			});
+		});
+
+		// Add metadata in different formats to zipEntries array
+		zipEntries.push({
+			filename: 'metadata.xml',
+			type: 'string',
+			value: convertObjectToXml(limitedObjectMetadata),
+		});
+		zipEntries.push({
+			filename: 'metadata.csv',
+			type: 'string',
+			value: convertObjectToCsv(limitedObjectMetadata),
+		});
+		zipEntries.push({
+			filename: 'metadata.json',
+			type: 'string',
+			value: JSON.stringify(limitedObjectMetadata, null, 2),
 		});
 
 		// const outputStream = fs.createWriteStream(__dirname + '/example.zip');
@@ -94,19 +122,10 @@ export class NewspapersController {
 			zlib: { level: 9 }, // Sets the compression level.
 		});
 
-		res.on('close', () => {
-			console.log(archive.pointer() + ' total bytes');
-			console.log('archiver has been finalized and the output file descriptor has closed.');
-		});
-
-		res.on('end', () => {
-			console.log('Data has been drained');
-		});
-
 		res.on('warning', (err) => {
 			if (err.code === 'ENOENT') {
 				// log warning
-				console.log('zip stream warning: ' + err);
+				console.warn('zip stream warning: ' + err);
 			} else {
 				// throw error
 				throw err;
@@ -114,14 +133,25 @@ export class NewspapersController {
 		});
 
 		res.set({
-			'Content-Disposition': `attachment; filename=${'metadata-' + id}.zip`,
+			'Content-Disposition': `attachment; filename=${'newspaper-' + id}.zip`,
 		});
-		res.attachment(`${'metadata-' + id}.zip`);
+		res.attachment(`${'newspaper-' + id}.zip`);
 		archive.pipe(res);
 
-		archive.append(metadataXml, { name: 'metadata.xml' });
-		archive.append(metadataCsv, { name: 'metadata.csv' });
-		archive.append(metadataJson, { name: 'metadata.json' });
+		// Append all zipEntries to the zip archive
+		await mapLimit(zipEntries, 5, async (entry) => {
+			return new Promise<void>((resolve) => {
+				if (entry.type === 'url') {
+					https.get(entry.value, (urlStream) => {
+						archive.append(urlStream, { name: entry.filename });
+						resolve();
+					});
+				} else {
+					archive.append(entry.value, { name: entry.filename });
+					resolve();
+				}
+			});
+		});
 
 		await archive.finalize();
 	}
