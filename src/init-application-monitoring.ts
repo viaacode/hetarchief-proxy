d9ed338e0dc4f825bfe5b91df3841cdac70c7169
Bert Verhelst <verhelstbert@gmail.com>
2025-06-27 15:52:07 +0200

    fix(observability): disable application perf monitoring on localhost

--- src/init-application-monitoring.ts
+++ src/init-application-monitoring.ts
@@ -1,4 +1,6 @@
 // jira ticket: https://meemoo.atlassian.net/browse/ARC-2791
 // dev docs: https://www.elastic.co/docs/reference/apm/agents/nodejs/express
 // dashboard: http://do-prd-elk-01.do.viaa.be:5601/app/observability/overview
-require('elastic-apm-node').start();
+if (process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test') {
+	require('elastic-apm-node').start();
+}
