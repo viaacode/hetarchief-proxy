02cb0aeac17b2f9954dbfa53485f115a2c0b2f47
Bert Verhelst <verhelstbert@gmail.com>
2026-02-06 19:47:39 +0100

    fix(tos): cache TOS last updated endpoint

--- src/modules/tos/controllers/tos.controller.ts
+++ src/modules/tos/controllers/tos.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Inject, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import type { Cache } from 'cache-manager';
 import { TosService } from '../services/tos.service';
 import type { Tos } from '../types';
+
+const CACHE_KEY_GET_TOS_LAST_UPDATED = 'CACHE_KEY_GET_TOS_LAST_UPDATED';
 
 @ApiTags('Tos')
 @Controller('tos')
 export class TosController {
 	private logger: Logger = new Logger(TosController.name, { timestamp: true });
 
-	constructor(private tosService: TosService) {}
+	constructor(
+		private tosService: TosService,
+		@Inject(CACHE_MANAGER) private cacheManager: Cache
+	) {}
 
 	@Get()
 	public async getTos(): Promise<Tos> {
-		const tos = await this.tosService.getTosLastUpdatedAt();
-		return tos;
+		return await this.cacheManager.wrap(
+			CACHE_KEY_GET_TOS_LAST_UPDATED,
+			() => this.tosService.getTosLastUpdatedAt(),
+			// cache for 60 minutes
+			3_600_000
+		);
 	}
 }
--- src/modules/tos/tos.module.ts
+++ src/modules/tos/tos.module.ts
@@ -1,12 +1,18 @@
 import { DataModule } from '@meemoo/admin-core-api';
 import { forwardRef, Module } from '@nestjs/common';
 
+import { CacheModule } from '@nestjs/cache-manager';
 import { TosController } from './controllers/tos.controller';
 import { TosService } from './services/tos.service';
 
 @Module({
 	controllers: [TosController],
-	imports: [forwardRef(() => DataModule)],
+	imports: [
+		forwardRef(() => DataModule),
+		CacheModule.register({
+			max: 1000,
+		}),
+	],
 	providers: [TosService],
 })
 export class TosModule {}
