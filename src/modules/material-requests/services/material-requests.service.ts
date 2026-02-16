ba46c7bcc2a1be9246218da484d7be19cb4bf804
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-02-16 10:36:46 +0100

    ARC-3398: fix null pointer

--- src/modules/material-requests/services/material-requests.service.ts
+++ src/modules/material-requests/services/material-requests.service.ts
@@ -1,5 +1,10 @@
 import { DataService, Locale, StillsObjectType, VideoStillsService } from '@meemoo/admin-core-api';
-import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException,
+} from '@nestjs/common';
 import { type IPagination, Pagination } from '@studiohyperdrive/pagination';
 import { compact, groupBy, intersection, isArray, isEmpty, isNil, kebabCase, noop, set } from 'lodash';
 
@@ -69,14 +74,17 @@ import {
 	UpdateMaterialRequestStatusMutation,
 	UpdateMaterialRequestStatusMutationVariables,
 } from '~generated/graphql-db-types-hetarchief';
-import { EmailTemplate, type MaterialRequestEmailInfo } from '~modules/campaign-monitor/campaign-monitor.types';
+import {
+	EmailTemplate,
+	type MaterialRequestEmailInfo,
+} from '~modules/campaign-monitor/campaign-monitor.types';
 
 import { CampaignMonitorService } from '~modules/campaign-monitor/services/campaign-monitor.service';
 import {
 	IeObjectAccessThrough,
 	IeObjectLicense,
-	IeObjectsVisitorSpaceInfo,
 	IeObjectType,
+	IeObjectsVisitorSpaceInfo,
 	SimpleIeObjectType,
 } from '~modules/ie-objects/ie-objects.types';
 import type { Organisation } from '~modules/organisations/organisations.types';
@@ -827,7 +835,7 @@ export class MaterialRequestsService {
 			reuseForm = null;
 		}
 
-		const objectId = graphQlMaterialRequest.intellectualEntity?.id;
+		const objectId = graphQlMaterialRequest.ie_object_id;
 		const objectSchemaIdentifier = graphQlMaterialRequest.intellectualEntity?.schema_identifier;
 		let objectAccessThrough: IeObjectAccessThrough[] = [];
 		let objectLicences: IeObjectLicense[] = [];
