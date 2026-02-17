ddd98c61115612ec6bfb005084980dd7ebdac5c8
Bert Verhelst <verhelstbert@gmail.com>
2026-02-17 14:25:18 +0100

    fix(ie-objects): fix route with param mistmatch for seo route

--- src/modules/ie-objects/controllers/ie-objects.controller.ts
+++ src/modules/ie-objects/controllers/ie-objects.controller.ts
@@ -54,12 +54,7 @@ import { IeObjectsService } from '../services/ie-objects.service';
 import { mapLimit } from 'blend-promise-utils';
 import { EventsService } from '~modules/events/services/events.service';
 import { LogEventType } from '~modules/events/types';
-import {
-	ALL_INDEXES,
-	IeObjectsSearchFilterField,
-	Operator,
-	OrderProperty,
-} from '~modules/ie-objects/elasticsearch/elasticsearch.consts';
+import { ALL_INDEXES, IeObjectsSearchFilterField, Operator, OrderProperty } from '~modules/ie-objects/elasticsearch/elasticsearch.consts';
 import { mapDcTermsFormatToSimpleType } from '~modules/ie-objects/helpers/map-dc-terms-format-to-simple-type';
 import { SessionUserEntity } from '~modules/users/classes/session-user';
 import { GroupName } from '~modules/users/types';
@@ -133,7 +128,7 @@ export class IeObjectsController {
 		return this.playerTicketService.getThumbnailUrl(thumbnailQuery.id, referer, ip);
 	}
 
-	@Get('seo/:id')
+	@Get('seo/:schemaIdentifier')
 	public async getIeObjectSeoById(
 		@Referer() referer: string,
 		@Ip() ip: string,
