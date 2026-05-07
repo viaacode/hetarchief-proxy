9e96845486ae5b007de5810488bb54d90724834b
Femke Reunes <femke.reunes@studiohyperdrive.be>
2026-05-07 08:23:41 +0200

    fix (material requests): Move archivation to separate controller since the LoggedInGuard will block reaching the endpoint

--- src/modules/material-requests/controllers/material-requests.controller.ts
+++ src/modules/material-requests/controllers/material-requests.controller.ts
@@ -47,7 +47,6 @@ import { Referer } from '~shared/decorators/referer.decorator';
 import { RequireAnyPermissions } from '~shared/decorators/require-any-permissions.decorator';
 import { RequireAllPermissions } from '~shared/decorators/require-permissions.decorator';
 import { SessionUser } from '~shared/decorators/user.decorator';
-import { ApiKeyGuard } from '~shared/guards/api-key.guard';
 import { LoggedInGuard } from '~shared/guards/logged-in.guard';
 import { EventsHelper } from '~shared/helpers/events';
 import { MaterialRequestsService } from '../services/material-requests.service';
@@ -392,34 +391,6 @@ export class MaterialRequestsController {
 			console.error(error);
 
 			throw error;
-		}
-	}
-
-	/**
-	 * Will check if there are any material requests ready for archivation
-	 * Should be triggered once every day
-	 */
-	@Post('archivation')
-	@ApiOperation({
-		description: 'trigger the poller to check for material requests that can be archived',
-	})
-	@UseGuards(ApiKeyGuard)
-	public async checkMaterialRequestsForArchivation(): Promise<{ message: 'checking' }> {
-		try {
-			this.materialRequestsService
-				.checkAllReadyForArchivation()
-				.then(noop)
-				.catch((err) => {
-					console.log(
-						new CustomError(
-							'Error during checkMaterialRequestsForArchivation => checkAllReadyForArchivation cron',
-							err
-						)
-					);
-				});
-			return { message: 'checking' };
-		} catch (err) {
-			throw new CustomError('Error checking material requests Archivation', err);
 		}
 	}
 
