91ae2d7874d161e979711be873338fe6b6c96459
Bert Verhelst <verhelstbert@gmail.com>
2026-07-08 17:07:51 +0200

    feat(INFRA-1795): bump nestjs to v10 + add rate limiter with redis
    https://meemoo.atlassian.net/browse/INFRA-1795

--- src/shared/exceptions/duplicate-key.exception.ts
+++ src/shared/exceptions/duplicate-key.exception.ts
@@ -4,10 +4,7 @@ export class DuplicateKeyException extends HttpException {
 	public data: any;
 
 	constructor(data: any) {
-		super(
-			HttpException.createBody({ message: 'Duplicate Key Exception', data }),
-			HttpStatus.INTERNAL_SERVER_ERROR
-		);
+		super({ message: 'Duplicate Key Exception', data }, HttpStatus.INTERNAL_SERVER_ERROR);
 		this.data = data;
 	}
 }
